5b1a297a1da2d081df8dbcab596839caa46a916f
Dalibor Svonavec <xsvonav@fi.muni.cz>
2024-12-13 13:45:09 +0100

    fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,7 +21,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(cors({
-  origin: 'https://sc01.fi.muni.cz/portal',
+  origin: 'https://sc01.fi.muni.cz/portal', // change to your domain
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
